25fd2d90032635fcd5c36eb5f584f17278d8fb5b
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-05-10 23:56:40 +0200

    sidebar__top done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Sidebar.css";
+import { Avatar } from "@mui/material";
+import banner from "./assets/banner.jpg";
+import me from "./assets/me.jpg";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Je suis une Sidebar</h1>
+            <div className="sidebar__top">
+                <img src={banner} alt="banner" />
+                <Avatar className="sidebar__avatar" src={me} />
+                <h2>Nicolas LOUIS</h2>
+                <h4>Développeur web</h4>
+            </div>
+            <div className="sidebar__stats">
+                <p>Relations</p>
+                <p className="sidebar__statNumber">4</p>
+            </div>
+
+            <div className="sidebar__bottom">
+                <p></p>
+            </div>
         </div>
     );
 }
