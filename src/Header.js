3e91b545a4dad9be5d8c06436490886bd3524e86
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-05-10 14:12:57 +0200

    first commit, header work

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img
+                    src="https://img.icons8.com/fluency/48/000000/linkedin.png"
+                    alt="linkedin_icon"
+                />
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text" />
+                </div>
+            </div>
+            <div className="header__right"></div>
+        </div>
+    );
+}
+
+export default Header;
