43ba3a11bb38392d426768c672af9696b15c84d4
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-05-10 23:14:20 +0200

    header done, beginning sidebar

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import linkedinLogo from "./assets/linkedinLogo.png";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import SmsIcon from "@mui/icons-material/Sms";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import me from "./assets/me.jpg";
 
 function Header() {
     return (
@@ -13,7 +20,18 @@ function Header() {
                     <input type="text" />
                 </div>
             </div>
-            <div className="header__right"></div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Accueil" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="Réseau" />
+
+                <HeaderOption
+                    Icon={BusinessCenterIcon}
+                    title="Offres d'emploi"
+                />
+                <HeaderOption Icon={SmsIcon} title="Messagerie" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar={me} title="Vous" />
+            </div>
         </div>
     );
 }
