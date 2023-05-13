a91aa4dfa72a21ce6e3a4075d29649f8cdb26a70
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-05-13 18:38:35 +0200

    Redux integration OK

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+    const user = useSelector(selectUser);
     return (
         <div className="app">
             <Header />
 
-            {/* App Body */}
-            <div className="app__body">
-                <Sidebar />
-                <Feed />
-            </div>
-            {/* Widgets */}
+            {!user ? (
+                <Login />
+            ) : (
+                <div className="app__body">
+                    <Sidebar />
+                    <Feed />
+                    {/* Widgets */}
+                </div>
+            )}
         </div>
     );
 }
