5b5a9658dac87d5a4b1de943ff8a4307ffd53cdb
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-05-14 12:06:05 +0200

    Login component, all fields created

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Login.css";
+import linkedin from "./assets/linkedin.svg";
 
 function Login() {
-    return <div className="login"></div>;
+    const register = () => {};
+    return (
+        <div className="login">
+            <img src={linkedin} alt="Linkedin logo" />
+            <form>
+                <input placeholder="Nom complet" type="text" />
+                <input placeholder="Photo de profil" type="text" />
+                <input placeholder="Email" type="email" />
+                <input placeholder="Mot de passe" type="password" />
+                <button>Se connecter</button>
+            </form>
+
+            <p>
+                Nouveau sur Linkedin?
+                <span className="login__register" onClick={register}>
+                    S'inscrire
+                </span>
+            </p>
+        </div>
+    );
 }
 
 export default Login;
