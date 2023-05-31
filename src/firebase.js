9a59d3067a24d172882ccdcf5bec71bb033dfed5
Nicolas LOUIS <nlouis_74_@hotmail.com>
2023-05-31 15:05:57 +0200

    firebase storage export added

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAE9iqBipV3jj-VmpLTTKsq7VjE3icHFzQ",
@@ -9,9 +10,11 @@ const firebaseConfig = {
     storageBucket: "linkedin-clone-dd0ab.appspot.com",
     messagingSenderId: "225761845242",
     appId: "1:225761845242:web:6c9e5866ee5d502e5d8e43",
+    // storageBucket: "gs://linkedin-clone-dd0ab.appspot.com",
 };
 
 const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
